1a45f472b012215608710f07d9235a3e8ba68245
David Griffiths <davidgg777@hotmail.com>
2021-03-03 10:34:50 +1100

    tableau rest api get workbooks and datasources

--- tableau/tableau_cicd.py
+++ tableau/tableau_cicd.py
@@ -11,55 +11,40 @@ password = os.getenv("TABLEAU_PASSWORD")
 tab_server = os.getenv("TABLEAU_SERVER") + "/"
 site_name = "wisemuffindev254014"
 
+workbook_list = ["Jaffle Shop - Orders by Customer", "Jaffle Shop - Orders by Week"]
+datasource_list = ["Orders (Prod)"]
+
+# initlaise server
 tableau_auth = TSC.TableauAuth(username, password, site_name)
 server = TSC.Server(tab_server)
 
-
-def get_datasources(datasource_list: [str]) -> [object]:
-    """returns a list of tableau datasource objects
-
-    output =
-    [
-        {"id": "String",
-        "name": "String",
-        "contentUrl": "String"
-        }
-    ]
-    """
-
-    with server.auth.sign_in(tableau_auth):
-        return [
-            ds for ds in TSC.Pager(server.datasources) if ds.name in datasource_list
-        ]
-
-
-def get_workbooks(workbook_list: [str]) -> [object]:
-    """returns a list of tableau workbook objects
-    workbooks_tableau = [
-        {
-            "id": "String",
-            "name": "String",
-            "contentUrl": "String"
-        }
-    ]
-    """
-
-    with server.auth.sign_in(tableau_auth):
-        return [wb for wb in TSC.Pager(server.workbooks) if wb.name in workbook_list]
-
-
-workbooks = get_workbooks(
-    ["Jaffle Shop - Orders by Customer", "Jaffle Shop - Orders by Week"]
-)
-
-datasources = get_datasources(["Orders (Prod)"])
-
-
 with server.auth.sign_in(tableau_auth):
 
-    workbook_id = workbooks[0].id
-    logging.info(workbook_id)
-    workbook_info = server.workbooks.get_by_id(workbook_id)
-    logging.info(workbook_info)
-    # workbooks.publish(workbook_item, file_path, publish_mode)
+    # get datasources and workbooks
+    workbooks = [wb for wb in TSC.Pager(server.workbooks) if wb.name in workbook_list]
+    datasources = [
+        ds for ds in TSC.Pager(server.datasources) if ds.name in datasource_list
+    ]
+    # download datasources and workbooks
+    for datasource in datasources:
+        logging.info(f"ID: {datasource.id}")
+        logging.info(f"ID: {datasource._id}")
+        logging.info(datasource)
+        server.datasources.download(
+            datasource_id=datasource._id,
+            filepath="./tableau/tab_downloads/datasources",
+            include_extract=True,
+            no_extract=None,
+        )
+
+    for workbook in workbooks:
+        logging.info(f"ID: {workbook.id}")
+        logging.info(f"ID: {workbook._id}")
+        logging.info(workbook)
+        server.workbooks.download(
+            workbook_id=workbook._id,
+            filepath="./tableau/tab_downloads/workbooks",
+            include_extract=True,
+            no_extract=None,
+        )
 
