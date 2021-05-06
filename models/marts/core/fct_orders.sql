b72467bcac38ffa53ac39ab5c0586ffe09571dc4
David Griffiths <davidgg777@hotmail.com>
2021-05-06 16:20:51 +1000

    testing out datafold

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -19,7 +19,7 @@ final as (
         orders.order_id,
         orders.customer_id,
         orders.order_date,
-        coalesce(order_payments.amount, 0) as amount
+        coalesce(order_payments.amount, 0) +200 as amount
     from orders
     left join order_payments using (order_id)
 )
