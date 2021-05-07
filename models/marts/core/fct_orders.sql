fab01219ca00693b59ec9517d2f36d127d53bd89
David Griffiths <davidgg777@hotmail.com>
2021-05-08 09:46:23 +1000

    datafold test

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -18,10 +18,12 @@ final as (
     select
         orders.order_id,
         orders.customer_id,
-        orders.order_date,
+        orders.order_date as date_ordered,
+        'dave is a awesome' as awesome_column,
         coalesce(order_payments.amount, 0) +10 as amount
     from orders
     left join order_payments using (order_id)
+    where coalesce(order_payments.amount, 0) < 30
 )
 select *
 from final
